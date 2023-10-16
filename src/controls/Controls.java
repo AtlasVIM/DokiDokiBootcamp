4c1d1c15c7641508a1af441ccd4edeee932589cf
ATLAS <caldasmartim@gmail.com>
2023-10-16 09:33:58 +0100

    Added inputs branch. Added controls class with keyboard inputs for up and down keys.

---
+++ src/controls/Controls.java
@@ -0,0 +1,49 @@
+package controls;
+
+import org.academiadecodigo.simplegraphics.keyboard.Keyboard;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardEvent;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardEventType;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardHandler;
+
+public class Controls implements KeyboardHandler{
+
+    public Controls() {
+    }
+
+    private void init() {
+        Keyboard keyboard = new Keyboard(this);
+
+        KeyboardEvent pressedUp = new KeyboardEvent();
+        pressedUp.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
+        pressedUp.setKey(KeyboardEvent.KEY_UP);
+        keyboard.addEventListener(pressedUp);
+
+        KeyboardEvent pressedDown = new KeyboardEvent();
+        pressedDown.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
+        pressedDown.setKey(KeyboardEvent.KEY_DOWN);
+        keyboard.addEventListener(pressedDown);
+
+    }
+
+    @Override
+    public void keyPressed(KeyboardEvent keyboardEvent) {
+        int key = keyboardEvent.getKey();
+
+        switch (key) {
+            case KeyboardEvent.KEY_UP:
+                break;
+            case KeyboardEvent.KEY_DOWN:
+                break;
+
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyboardEvent keyboardEvent) {
+
+    }
+}
+
+
+
+
